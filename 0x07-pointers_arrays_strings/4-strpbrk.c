6a60ae3f7bbe356d1fad99ac3072e65955b90275
Deborahjimmy <deborahjimmy.dj@gmail.com>
2022-04-26 19:06:59 +0100

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,25 +1,19 @@
-#include "mainn.h"
+#include "main.h"
+
 /**
- * _strpbrk -  searches a string for any of a set of bytes
- * @s:string
- * @accept:string
- * Return:char
+ * _strpbrk - finds first matching char in string
+ * @s: input string to search for matching char
+ * @accept: characters that could be matched
+ * Return: pointer to matching char
  */
+
 char *_strpbrk(char *s, char *accept)
 {
-int i;
+	unsigned int i, j;
 
-while (*s)
-{
-for (i = 0; accept[i]; i++)
-{
-	if (*s == accept[i])
-	{
-		return (s);
-	}
-
-}
-s++;
-}
-return ('\0');
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; accept[j] != '\0'; j++)
+			if (s[i] == accept[j])
+				goto exit;
+exit: return (s[i] != '\0' ? s + i : '\0');
 }
