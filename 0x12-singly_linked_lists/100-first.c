a9cbb0fe38193ccbae65a8dea90f93ed70ba693a
Deborahjimmy <deborahjimmy.dj@gmail.com>
2022-05-25 01:27:07 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include "lists.h"
 
-void printStatement(void) __attribute__ ((constructor));
-
+void test(void) __attribute__((constructor));
 /**
- * printStatement - Print out a statment, before main function
+ * test - function with constructor
+ *
+ * Description: print string
+ * Return: na
  */
-void printStatement(void)
+void test(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-}	
+}
